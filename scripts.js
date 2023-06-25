e69b6418d6405b97bef112259b1d4d50cc8dafe0
0RaffaDev <graffadev0@gmail.com>
2023-06-25 17:51:40 -0300

    Arreglo menu, empezando js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
-let coin = 0;
+window.onload=Inicio;
+var credito=Math.floor(Math.random()*4)+9;
+var imagenes= ["tigre_slot.png"];
+var premios= [3,2,3,2,2,3,6];
+var numeros_actuales=[];
+
+ 
+function escoger_numero(){
+    var azar=Math.floor(Math.random()*imagenes.length);
+    return azar;
+}
+
+function mostrar_imagen(num,im){
+document.getElementsByClassName("imagen")[num, im].get
+ElementsByTagName("img")[8].src="img/"+imagenes
+
+}
+
+function comparar(){
+
+}
+
+function actualizar(){
+
+}
+
+function mostrar_mensaje(){
+
+}
+
+function cerrar(){
+
+}
+
+function sonar(){
+
+}
+
 
 
