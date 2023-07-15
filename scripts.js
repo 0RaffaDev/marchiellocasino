5278c613512c20440ccb38b1cc4393e4d2163ecf
0RaffaDev <graffadev0@gmail.com>
2023-07-15 18:31:53 -0300

    Angry birds

--- scripts.js
+++ scripts.js
@@ -1,31 +1,36 @@
-window.onload=inicio; 
+
 var credito=Math.floor(Math.random()*4)+9;
-var imagenes= ["tigre_slot.png"];
+var imagenes= ["tigre_slot.png", "7.png"];
 var premios= [3,2,3,2,2,3,6];
 var numeros_actuales=[];
 
-function inicio(){
-    document.getElementById("tirar").onclick=lanzar();
+/*function inicio(){
+    document.getElementById("tirar").click=lanzar();
 }
- 
+*/ 
+
+function mantenimiento(){
+alert("JUEGO EN MANTENIMIENTO, DISCULPE LAS MOLESTIAS")
+
+}
 function escoger_numero(){
 
     var azar=Math.floor(Math.random()*imagenes.length);
-    return azar;    
+    return azar;
 }
 
 function lanzar(){
     numeros_actuales=[];
     for (let k=0; k<document.getElementsByClassName("boton").length;k++){
     numeros_actuales.push(escoger_numero());
-    mostrar_imagen(k,numeros_actuales[k])
-    
+    mostrar_imagen(k,numeros_actuales[k]);
     }
-    alert(numeros_actuales);
+
 
 }
 function mostrar_imagen(num,im){
 document.getElementsByClassName("imagen")[num, im].getElementsByTagName("img")[0].src="tigre_slot.png";
+document.getElementsByClassName("imagen")[num, im].getElementsByTagName("img")[1].src="7.png";
 
 }
 
